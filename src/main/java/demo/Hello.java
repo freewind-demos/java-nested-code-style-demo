3fe0e45c64ed5ad89505eaf11fb93160b7565ed2
freewind <nowindlee@gmail.com>
2018-07-14 13:39:01 +0800

    menu demo

--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -1,12 +1,20 @@
 package demo;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Hello extends Application {
+
+    private Label label = new Label("Hello, JavaFX!");
+
     public static void main(String[] args) {
         launch(args);
     }
@@ -14,9 +22,20 @@ public class Hello extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Hello");
-        Label label = new Label("Hello, JavaFX!");
-        StackPane root = new StackPane();
-        root.getChildren().add(label);
+        VBox root = new VBox() {{
+            getChildren().add(new MenuBar() {{
+                getMenus().add(new Menu("Menu") {{
+                    getItems().add(new MenuItem("change content") {{
+                        this.setOnAction(new EventHandler<ActionEvent>() {
+                            public void handle(ActionEvent event) {
+                                label.setText("Changed!");
+                            }
+                        });
+                    }});
+                }});
+            }});
+            getChildren().add(label);
+        }};
         primaryStage.setScene(new Scene(root, 300, 250));
         primaryStage.show();
     }
